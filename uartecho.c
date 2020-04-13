1921b0704893d3cebba64672f842f8b864960639
Rucha Borwankar <55333982+ruchaborwankar@users.noreply.github.com>
2020-04-12 20:29:40 -0600

    Update uartecho.c

--- uartecho.c
+++ uartecho.c
@@ -1,9 +1,11 @@
 /*******************************************************************************************
 			Rucha Borwankar
 			final aesd uart example code for testing echo
-Simple send message example for communicating with the UART that is connected to a desktop PC. 
+Simple send message example for communicating with the UART that is connected to a desktop PC.
+References:-
 https://github.com/derekmolloy/exploringBB/blob/version2/chp08/uart/uartEchoC/BBBEcho.c
 Exploring Beaglebone black example code for echo to computer
+https://www.cmrr.umn.edu/~strupp/serial.html#2_5_4
 ***********************************************************************************************/
 
 #include<stdio.h>
