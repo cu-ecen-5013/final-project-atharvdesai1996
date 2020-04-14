ce88bd70d89d716eb1a9f513ff9e8d35f177d9fa
aakshajaywant <55064839+aakshajaywant@users.noreply.github.com>
2020-04-14 05:41:13 -0600

    final

---
+++ TMP_sencode/uart_echo_final/Debug/tmp102.d
@@ -0,0 +1,61 @@
+# FIXED
+
+tmp102.obj: ../tmp102.c
+tmp102.obj: ../tmp102.h
+tmp102.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdint.h
+tmp102.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_stdint40.h
+tmp102.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/stdint.h
+tmp102.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/cdefs.h
+tmp102.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_types.h
+tmp102.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_types.h
+tmp102.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_stdint.h
+tmp102.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_stdint.h
+tmp102.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdbool.h
+tmp102.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdio.h
+tmp102.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_ti_config.h
+tmp102.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/linkage.h
+tmp102.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdarg.h
+tmp102.obj: C:/ti/TivaWare_C_Series-Now/inc/hw_memmap.h
+tmp102.obj: C:/ti/TivaWare_C_Series-Now/driverlib/debug.h
+tmp102.obj: C:/ti/TivaWare_C_Series-Now/driverlib/gpio.h
+tmp102.obj: C:/ti/TivaWare_C_Series-Now/driverlib/interrupt.h
+tmp102.obj: C:/ti/TivaWare_C_Series-Now/driverlib/pin_map.h
+tmp102.obj: C:/ti/TivaWare_C_Series-Now/driverlib/rom.h
+tmp102.obj: C:/ti/TivaWare_C_Series-Now/driverlib/rom_map.h
+tmp102.obj: C:/ti/TivaWare_C_Series-Now/driverlib/sysctl.h
+tmp102.obj: C:/ti/TivaWare_C_Series-Now/driverlib/uart.h
+tmp102.obj: C:/ti/TivaWare_C_Series-Now/inc/tm4c1294ncpdt.h
+tmp102.obj: C:/ti/TivaWare_C_Series-Now/driverlib/i2c.h
+tmp102.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/math.h
+tmp102.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_defs.h
+tmp102.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_limits.h
+
+../tmp102.c: 
+../tmp102.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_stdint40.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/cdefs.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_types.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_types.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdbool.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdio.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_ti_config.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/linkage.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdarg.h: 
+C:/ti/TivaWare_C_Series-Now/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-Now/driverlib/debug.h: 
+C:/ti/TivaWare_C_Series-Now/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-Now/driverlib/interrupt.h: 
+C:/ti/TivaWare_C_Series-Now/driverlib/pin_map.h: 
+C:/ti/TivaWare_C_Series-Now/driverlib/rom.h: 
+C:/ti/TivaWare_C_Series-Now/driverlib/rom_map.h: 
+C:/ti/TivaWare_C_Series-Now/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-Now/driverlib/uart.h: 
+C:/ti/TivaWare_C_Series-Now/inc/tm4c1294ncpdt.h: 
+C:/ti/TivaWare_C_Series-Now/driverlib/i2c.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/math.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_defs.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_limits.h: 
