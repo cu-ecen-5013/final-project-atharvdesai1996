486aabf4f76481e84028336b402d48528dc11d3e
ruchaborwankar <Rucha.Borwankar@colorado.edu>
2020-04-16 20:57:55 -0600

    4800 baud

--- writetotiva.c
+++ writetotiva.c
@@ -24,7 +24,7 @@ int main(){
 
    	// Set up the communications options:
    	//   9600 baud, 8-bit, enable receiver, no modem control lines
-   	options.c_cflag = B9600 | CS8 | CREAD | CLOCAL;
+   	options.c_cflag = B4800 | CS8 | CREAD | CLOCAL;
   	options.c_iflag = IGNPAR | ICRNL;    //ignore partity errors, CR -> newline
    	tcflush(file, TCIFLUSH);             //discard file information not transmitted
    	tcsetattr(file, TCSANOW, &options);  //changes occur immmediately
